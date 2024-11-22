270cfd774c6f940531d31b479f4b2d7dc13711a8
rasyadlubisdev <rasyadloebisa@gmail.com>
2024-11-22 08:55:03 +0700

    change endpoint api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ export const predictImage = async (imageFile) => {
     const formData = new FormData()
     formData.append('file', imageFile)
   
-    const response = await fetch('https://fldqzq-7860.csb.app/predict/', {
+    const response = await fetch('https://yg8lyz-7860.csb.app/predict/', {
       method: 'POST',
       body: formData,
     })
@@ -14,7 +14,7 @@ export const predictImage = async (imageFile) => {
     const formData = new FormData()
     formData.append('file', imageFile)
   
-    const response = await fetch('https://fldqzq-7860.csb.app/predict-large/', {
+    const response = await fetch('https://yg8lyz-7860.csb.app/predict-large/', {
       method: 'POST',
       body: formData,
     })
